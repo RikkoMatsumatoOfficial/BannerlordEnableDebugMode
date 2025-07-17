b24fc75fad5fb79a229b47837001b76ce55a11f2
Rikko Matsumato(Real Name Is Hlib Pugach(Arthur)) <kuttimsx@hotmail.com>
2025-07-17 16:32:16 +0300

    Fixed Reading File engine_config.txt
    Fixed Reading File engine_config.txt!!!

--- engineconfig.py
+++ engineconfig.py
@@ -8,7 +8,7 @@ def SetEngineConfigAsDebug():
     engineconf = str("engine_config.txt")
     if p.Path(engineconf):
         print("Succesfully Founded File!!!")
-        with open(engineconf) as f:
+        with open(engineconf, "r") as f:
             for file in f:
                 print(file)
                 confirm = input("Do you Confirm this Configuration? ")
@@ -19,7 +19,7 @@ def SetEngineConfigAsDebug():
                         print("Founded this File(Original engine_config.txt)!!!")
                         os.remove("C:\\Users\\{}\\Documents\\Mount and Blade II Bannerlord".format(username) + "\\Configs\\engine_config.txt")
                         ud.copy("engine_config.txt", "C:\\Users\\{}\\Documents\\Mount and Blade II Bannerlord".format(username) + "\\Configs\\engine_config.txt")
-                        file_attribution.SetFileAttribute("C:\\Users\\{}\\Documents\\Mount and Blade II Bannerlord".format(username) + "\\Configs\\engine_config.txt", 0x1) # 0x1 is Only Read File!!!
+                        file_attribution.SetFileAttribute(bytes("C:\\Users\\{}\\Documents\\Mount and Blade II Bannerlord".format(username) + "\\Configs\\engine_config.txt", "utf-8"), 0x1) # 0x1 is Only Read File!!!
                         os.remove("engine_config.txt")
                         print("Successfully Added Debug Mode!!! Created by RikkoMatsumatoOfficial!!!")
                         exit(443)
