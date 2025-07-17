ca60692a461f5675d780f51a4e3a4079f43a47fe
Rikko Matsumato(Real Name Is Hlib Pugach(Arthur)) <kuttimsx@hotmail.com>
2025-07-17 16:32:58 +0300

    Update FileAttr.py

--- FileAttr.py
+++ FileAttr.py
@@ -1,5 +1,9 @@
 import ctypes as c
-
+from os import _exit as exit
 def SetFileAttribute(name, attributes : int):
-    kernel32 = c.CDLL("Kernel32.dll")
-    return kernel32.SetFileAttributesA(name, attributes)
+    try:
+        kernel32 = c.CDLL("Kernel32.dll")
+        return kernel32.SetFileAttributesA(name, attributes)
+    except(PermissionError, Exception):
+        print("File is Used by Any Program or It is Read-Only File!!! Pls Close All Unused Program or Used Program what opened engine_config.txt!!!")
+        exit(3221)
